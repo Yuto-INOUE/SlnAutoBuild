8e5c20fbe86ed09d9270e4793a83472983f25179
井上 雄斗 <dsyuuto@gmail.com>
2020-09-28 18:14:12 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App.SlnAutoBuild
+{
+	static class Program
+	{
+		private static void Main(string[] args)
+		{
+			SlnAutoBuild.Execute();
+		}
+	}
+}
---
+++ SlnAutoBuild.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace App.SlnAutoBuild
+{
+	internal static class SlnAutoBuild
+	{
+		public static void Execute()
+		{
+			foreach (var p in GetTargetSlnFilePaths())
+			{
+				var process = Process.Start(
+					new ProcessStartInfo
+					{
+						FileName = Properties.Resources.MSBuildExePath,
+						Arguments = string.Format("{0} -t:rebuild -p:configuration=Debug -maxcpucount:4", p),
+					});
+
+				process?.WaitForExit();
+			}
+		}
+
+		private static IEnumerable<string> GetTargetSlnFilePaths()
+		{
+			return Directory.EnumerateFiles(Properties.Resources.ExploreTargetDirPath, "*.sln", SearchOption.AllDirectories);
+		}
+	}
+}
